5cdddbbf2dafcb07b5fe6ebb75718216f95f3907
JonasKaruzas <jonas.karuzas@gmail.com>
2021-10-14 20:42:27 +0300

    works, but no classes

--- tictaktoe.rb
+++ tictaktoe.rb
@@ -1,44 +1,59 @@
+def display
+  puts " #{$game_board[0]} | #{$game_board[1]} | #{$game_board[2]} "
+  puts "---|---|---"
+  puts " #{$game_board[3]} | #{$game_board[4]} | #{$game_board[5]} "
+  puts "---|---|---"
+  puts " #{$game_board[6]} | #{$game_board[7]} | #{$game_board[8]} "
+end
 
-class Game
-  @@game_board = [" "," "," "," "," "," "," "," "," "]
-  
-  def display
-    puts " #{@@game_board[0]} | #{@@game_board[1]} | #{@@game_board[2]} "
-    puts "---|---|---"
-    puts " #{@@game_board[3]} | #{@@game_board[4]} | #{@@game_board[5]} "
-    puts "---|---|---"
-    puts " #{@@game_board[6]} | #{@@game_board[7]} | #{@@game_board[8]} "
+def get_player_letter(player)
+  if player == "Player1"
+    puts "#{player}, pick a letter"
+    return(gets.chomp)
+  else
+    puts "#{player}, pick a letter, not #{$player_letters[0]}"
+    return(gets.chomp)
   end
+end
+
+def checkWin? 
+  for i in 0..1
+    player = $player_letters[i]
+    if $game_board[0] == player && $game_board[1] == player && $game_board[2] == player ||
+      $game_board[3] == player && $game_board[4] == player && $game_board[5] == player ||
+      $game_board[6] == player && $game_board[7] == player && $game_board[8] == player ||
+      $game_board[0] == player && $game_board[3] == player && $game_board[6] == player ||
+      $game_board[1] == player && $game_board[4] == player && $game_board[7] == player ||
+      $game_board[2] == player && $game_board[5] == player && $game_board[8] == player ||
+      $game_board[0] == player && $game_board[4] == player && $game_board[8] == player ||
+      $game_board[2] == player && $game_board[4] == player && $game_board[6] == player 
+      
+      puts "#{player} won!"
+      $game_won = true
+    end
+  end
+end
+
+$game_board = ["0","1","2","3","4","5","6","7","8"]
+$player_letters = []
+
+$player_letters[0] = get_player_letter("Player1")
+$player_letters[1] = get_player_letter("Player2")
+
+$game_won = false
+
+display()
+
+until $game_won do
+  puts "Player1 (#{$player_letters[0]}), choose a number"
+  $game_board[gets.chomp.to_i] = $player_letters[0]
   
+  display()
+  checkWin?()
 
-end
-
-g = Game.new()
-g.display
-
-class Player
-  def initialize(name)
-    @name = name
-end
-
-def get_name
-  puts "Player, enter your name"
-  return (get_s.chomp)
-end
-
-# Create board
-# Create player1
-# Create player2
-
-# Show board
-# Ask player1 for move
-# if cell is not empty > try again
-# update board
-#   checkWin?
-
-# Show board
-# Ask player2 for move
-# if cell is not empty > try again
-# update board
-#   checkWin?
+  puts "Player2 (#{$player_letters[1]}), choose a number"
+  $game_board[gets.chomp.to_i] = $player_letters[1]
   
+  display()
+  checkWin?()
+end
